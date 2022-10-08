e0324de492d84ceb1ba78e1462073621d3869926
Valerie Espinosa <vne@tamu.edu>
2022-10-08 12:54:41 -0500

    create file to upload all libraries needed for project

---
+++ Functions/Libraries.R
@@ -0,0 +1,14 @@
+#Save and call all Files in same area
+
+#inset all libraries needed
+packages <- c("tidyverse", "ggplot2" , "sf", "mapview", "astsa", "readr")
+
+#install packages that are not already on computer
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+#load packages
+invisible(lapply(packages, library, character.only = TRUE))
+
