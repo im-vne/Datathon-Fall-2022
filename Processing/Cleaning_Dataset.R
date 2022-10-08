7fc0fd6c2048754f4405104288c9373d1930e028
Megan Tung <meganatung@tamu.edu>
2022-10-08 13:58:46 -0500

    loaded in csvs into a list

--- Processing/Cleaning_Dataset.R
+++ Processing/Cleaning_Dataset.R
@@ -5,3 +5,9 @@
 # set path - changeable
 path= "/Datathon-Fall-2022/Dataset/all_data_v2"
 
+# read all csvs
+filenames = list.files(path, pattern="*.csv", full.names=TRUE)
+dta = lapply(filenames, read.csv)
+
+# change names for easier access
+names(dta) = paste0("data_", 1:213)
