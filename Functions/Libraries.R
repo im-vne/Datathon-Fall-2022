3c27731fa74b7cf98d2a5fad4e8026b718db2749
Valerie Espinosa <vne@tamu.edu>
2022-10-08 13:10:16 -0500

    add ggmap package

--- Functions/Libraries.R
+++ Functions/Libraries.R
@@ -1,7 +1,7 @@
 #Save and call all Files in same area
 
 #inset all libraries needed
-packages <- c("tidyverse", "ggplot2" , "sf", "mapview", "astsa", "readr")
+packages <- c("tidyverse", "ggplot2" , "sf", "mapview", "astsa", "readr", "ggmap")
 
 #install packages that are not already on computer
 installed_packages <- packages %in% rownames(installed.packages())
