6d7b369e112aa0b35f82f5730f076d3bd1f387b7
Megan Tung <meganatung@tamu.edu>
2022-10-08 19:13:40 -0500

    added total CO2 emissions of each plant to sites table based on average CO2 emissions of each engine

--- Functions/Map_Plot.R
+++ Functions/Map_Plot.R
@@ -1,18 +1,22 @@
 # Name: Valerie Espinosa
 #Create  a map with the coordinates of each plot. 
 
+# CO2 averages
 engine_avg = c()
 for(i in 1:length(dta)){
 engine_mean = mean(na.omit(dta[[i]][,5]))
 engine_avg = c(engine_avg,engine_mean)
 }
 
-engine_data %>% group_by(PLANT_NAME) %>% count(PLANT_NAME) 
+engines = engines%>% mutate(AVG_CO2 = engine_avg)
+engines = engines %>% arrange(PLANT_NAME) %>% group_by(PLANT_NAME) %>% mutate(TOT_CO2 = sum(AVG_CO2))
 
 #create vector out of tibble from engine_data
+###  these stay in order
+# unique(engines$TOT_CO2)
+#assign to respective sites
 
-#assign vector as a way for for loop to attatch values?
-
+sites = sites %>% arrange(PLANT_NAME) %>% mutate(TOT_CO2 = unique(engines$TOT_CO2)) 
 
 
 points_sf <- st_as_sf(site_data, coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
